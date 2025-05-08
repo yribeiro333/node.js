aec02afff4fbf2c2c4d6a5847f085c97f5c3062a
Henrique <yribeiro333br@gmail.com>
2025-05-08 17:51:41 -0300

    ✨ feat(auth): add login route with password check and JWT token generation 🔐

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const router = express.Router()
 
-// Cadastro
+const JWT_SECRET = process.env.JWT_SECRET
+
+// CADASTRO
+
 router.post('/cadastro', async (req, res) => {
 
     try {
@@ -23,6 +27,43 @@ router.post('/cadastro', async (req, res) => {
             }
         })
         res.status(201).json(userDB)
+    } catch (err) {
+        res.status(500).json({ message: 'Erro no servidor, tente novamente' })
+    }
+})
+
+// LOGIN
+
+router.post('/login', async (req, res) => {
+
+    try {
+        const userInfo = req.body
+
+        // BUSCA O USUÁRIO NO BANCO DE DADOS
+
+        const user = await prisma.user.findUnique({
+            where: { email: userInfo.email },
+        })
+
+        // VERIFICA SE O USUÁRIO EXISTE DENTRO NO BANCO
+
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' })
+        }
+
+        // COMPARA A SENHA DO BANCO COM A QUE O USUÁRIO DIGITOU
+
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Senha invalida' })
+        }
+
+        // GERAR O TOKEN JWT
+
+        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1m'})
+
+        res.status(200).json(token)
     } catch (err) {
         res.status(500).json({ message: 'Erro no servidor, tente novamente' })
     }
