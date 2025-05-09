34b65860ea55a534e79ccde4b89bdd1a432c93c7
Henrique <yribeiro333br@gmail.com>
2025-05-09 18:47:29 -0300

    ✨ feat(routes): add private route for listing users with Prisma integration

---
+++ routes/private.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const router = express.Router()
+const prisma = new PrismaClient()
+
+router.get('/listar-usuarios', async (req, res) => {
+    try {
+
+        const users = await prisma.user.findMany()
+
+        res.status(200).json({ message: 'Usuarios listados com sucesso', users })
+    } catch (err) {
+        res.status(500).json({ message: 'Falha no servidor ' })
+    }
+})
+
+export default router
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
-
+import privateRoutes from './routes/private.js'
 
 const app = express()
 app.use(express.json())
 
 app.use('/', publicRoutes)
+app.use('/', privateRoutes)
 
 
 
