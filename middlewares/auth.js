9b4458094135b76c1080fcc2f13581c1755700b0
Henrique <yribeiro333br@gmail.com>
2025-05-19 16:52:19 -0300

    🔧 refactor(auth): format code for better readability and remove redundant next call

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ const auth = (req, res, next) => {
 
     const token = req.headers.authorization
 
-    if(!token) {
+    if (!token) {
         return res.status(401).json({ message: 'Acesso negado' })
 
     }
@@ -14,10 +14,11 @@ const auth = (req, res, next) => {
     try {
         const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
 
-        console.log(decoded)
+        req.userId = decoded.id
 
-    }catch(err){
-        return res.status(401).json({ message: 'Token Inválido'})
+        next()
+    } catch (err) {
+        return res.status(401).json({ message: 'Token Inválido' })
     }
     next()
 }
--- routes/private.js
+++ routes/private.js
@@ -6,7 +6,6 @@ const prisma = new PrismaClient()
 
 router.get('/listar-usuarios', async (req, res) => {
     try {
-
         const users = await prisma.user.findMany()
 
         res.status(200).json({ message: 'Usuarios listados com sucesso', users })
